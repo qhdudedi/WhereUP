30c398cd24878eb940e359022a593d331dfe516c
qhdudedi <boyoung035@gmail.com>
2024-04-29 14:39:57 +0900

    Initial commit

---
+++ src/main/java/com/project/whereup/WhereUpApplication.java
@@ -0,0 +1,13 @@
+package com.project.whereup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/whereup/WhereUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.whereup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
