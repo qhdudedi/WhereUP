3e49d6a3b1a94b37548812ab1fb0f3e7f4914e50
qhdudedi <boyoung035@gmail.com>
2024-06-18 17:10:48 +0900

    Chore: Jenkins resetting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/deploy/*.jar $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl WhereUP | grep java | awk '{print $1}')
 
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+echo "현재 구동 중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
