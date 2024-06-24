a4e153c38a3f6919bb5f12feaf4b9a1e7637f9cc
qhdudedi <boyoung035@gmail.com>
2024-06-25 01:57:20 +0900

    chore: deploy 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,6 +20,17 @@ else
     sudo kill -15 $CURRENT_PID
     sleep 10
 fi
+CURRENT_PID=$(pgrep -fl WhereUP | grep java | awk '{print $1}')
+
+echo "현재 구동 중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    sudo kill -15 $CURRENT_PID
+    sleep 10
+fi
 
 echo "> 새 어플리케이션 배포"
 
